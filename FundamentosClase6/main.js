4e5521d3e998e402dd02fd99e1129221bd268e30
Karolay Cristina Quirós Jiménez <karolay.quiros@CETAV-B6-15.local>
2026-03-04 16:44:13 -0600

    Añadir archivos

---
+++ FundamentosClase6/main.js
@@ -0,0 +1,21 @@
+console.log("Hola Mundo!");
+var numberFloat = 15.60
+console.log(numberFloat + 10);
+
+let numberWithZero = '15.60';
+console.log(numberWithZero + 10);
+
+//Numeros +, -, *, /, <, >
+const suma = 3 * ((40 + 20) * 2);
+const otraSuma = suma + 74;
+console.log(otraSuma);
+
+//Strings
+let name = 'Karolay';
+let lastName = 'Quirós';
+console.log(name + ' ' + lastName);
+
+//Booleans
+true || false
+const num = 10;
+console.log(num > 2);
---
+++ FundamentosClase6/practica.js
@@ -0,0 +1,24 @@
+const lunes = 'Lunes';
+const martes = 'Martes';
+const miercoles = 'Miercoles';
+const jueves = 'Jueves';
+const viernes = 'Viernes';
+const sabado = 'Sabado';
+const domingo = 'Domingo';
+const hoy = 'Miercoles';
+const console1 = hoy == lunes;
+const console2 = hoy == martes;
+const console3 = hoy == miercoles;
+const console4 = hoy == jueves;
+const console5 = hoy == viernes;
+const console6 = hoy == sabado;
+const console7 = hoy == domingo;
+
+true || false
+console.log("Hoy es Lunes? " + console1);
+console.log("Hoy es Martes? " + console2);
+console.log("Hoy es Miercoles? " + console3);
+console.log("Hoy es Jueves? " + console4);
+console.log("Hoy es Viernes? " + console5);
+console.log("Hoy es Sabado? " + console6);
+console.log("Hoy es Domingo? " + console7);
